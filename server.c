cb5180f86a1a6323966a5993eaca260c94c25f91
maleakhiw <maleakhiw@student.unimelb.edu.au>
2018-04-17 16:56:18 +1000

    Modify server.c to finish creation, bind, listen, accept

--- server.c
+++ server.c
@@ -19,13 +19,16 @@
 /** Constant */
 #define ARGUMENT 3
 #define PORT_INDEX 1
+#define WEB_ROOT_PATH_INDEX 2
 #define MAX_RESERVED_PORT 1024
+#define BACKLOG 10
 
 int main(int argc, char *argv[]) {
-    int socket_descriptor, port_number;
-    char receive_buffer[1000], send_buffer[2000];
+    int socket_descriptor, port_number, new_socket_descriptor;
+    char receive_buffer[256], send_buffer[256];
+    char *web_root_path;
     struct sockaddr_in server_address, client_address;
-    socklen_t client;
+    socklen_t client_address_len;
 
     /* Initialisation */
     // Check that argument to command line is sufficient
@@ -36,11 +39,16 @@ int main(int argc, char *argv[]) {
 
     // Extract arguments from command line and make sure everything is valid
     port_number = atoi(argv[PORT_INDEX]);
+    web_root_path = argv[WEB_ROOT_PATH_INDEX];
     if (port_number < MAX_RESERVED_PORT) {
-        fprintf(stderr, "Port number is reserved, please choose ports above
-            1024\n");
+        fprintf(stderr, "Port number is reserved, please choose ports above 1024\n");
         exit(1);
     }
+
+    // Initialise address, buffers
+    memset(&server_address, '0', sizeof(server_address));
+    memset(receive_buffer, '0', 256);
+    memset(send_buffer, '0', 256);
 
     /* Create Socket */
     socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
@@ -52,18 +60,33 @@ int main(int argc, char *argv[]) {
     }
 
     /* Bind Socket */
-     // Create server address that will be used to listen incoming connection
-     memset(&server_address, '0', sizeof(server_address));
-     server_address.sin_family = AF_INET;
-     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-     server_address.sin_port = htons(port_number);
+    // Create server address that will be used to listen incoming connection
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(port_number);
 
-     // Bind the socket with address (IP and port number)
-     if (bind(socket_descriptor, (struct sockaddr *) &server_address,
-        sizeof(server_address)) < 0) {
-            perror("ERROR on binding process");
-            exit(1);
-     }
+    // Bind the socket with address (IP and port number)
+    if (bind(socket_descriptor, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
+        perror("ERROR on binding process");
+        exit(1);
+    }
+
+    /* Listen on Socket */
+    listen(socket_descriptor, BACKLOG);
+
+    /* Accept Client Connection */
+    client_address_len = sizeof(client_address);
+
+    // New socket descriptor will be used to send and receive later
+    new_socket_descriptor = accept(socket_descriptor, (struct sockaddr *) &client_address, &client_address_len);
+
+    // Check for ERROR
+    if (new_socket_descriptor < 0) {
+        perror("ERROR on accept");
+        exit(1);
+    }
+
+    printf("Success accepting %d\n", new_socket_descriptor);
 
     return 0;
 }
